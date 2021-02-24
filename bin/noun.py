432ba25f7fd60a3562eb2dd522f01e7b730fbcea
Pierre Amadio <amadio.pierre@gmail.com>
2021-02-24 11:00:32 +0100

    noun stuff

--- bin/noun.py
+++ bin/noun.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """
+https://www.translatum.gr/converter/beta-code.htm
+
  N = Noun (up to 3 columns)
    N1 = 1st declension (fem. in -H)
      N1A = stem ending in -A (fem.)
@@ -20,10 +22,33 @@ from packard import *
 class Noun(Packard):
   declension=None
 
+  def firstDeclension(self):
+    print("first declension")
+    if len(self.typeCode)==2:
+      print("1st declension (fem. in -η)")
+    else:
+      if self.typeCode[2]=='A':
+        print("A")
+      elif self.typeCode[2]=='M':
+        print("M")
+      elif self.typeCode[2]=='S':
+        print("S")
+      elif slef.typecode[2]=='T':
+        print("T")
+
+
+
   def parseRawCode(self):
     self.WordType="Noun"
     if len(self.typeCode)==1:
       self.declension=0
     else:
       self.declension=int(self.typeCode[1])
+      if self.declension==1:
+        self.firstDeclension()
+      if self.declension==2:
+        self.secondDeclension()
+      if self.declension==3:
+        self.thirdDeclension()
+    
 

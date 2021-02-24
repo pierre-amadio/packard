8b6261b203886e37d68a11d73b825b7ad2d84652
Pierre Amadio <amadio.pierre@gmail.com>
2021-02-24 10:39:59 +0100

    wrong order

--- bin/noun.py
+++ bin/noun.py
@@ -2,5 +2,12 @@
 from packard import *
 
 class Noun(Packard):
+  declension=None
+
   def parseRawCode(self):
     print("need to parse code",self.rawCode)
+    print("parse",self.parseCode)
+    print("type",self.typeCode)
+    self.WordType="Noun"
+    print(self.typeCode[1])
+
--- bin/packard.py
+++ bin/packard.py
@@ -12,6 +12,7 @@ class Packard:
   rawCode=None
   parseCode=None
   typeCode=None
+  wordType=None
 
   def __init__(self, rawcode):
     self.rawCode = rawcode
@@ -19,11 +20,10 @@ class Packard:
     if not test:
       print("hmmm,no dash in rawcode",self.rawCode)
     else:
-      self.parseCode=test.group(1)
-      self.typeCode=test.group(2)
+      self.typeCode=test.group(1)
+      self.parseCode=test.group(2)
 
     self.parseRawCode()
-    print("done")
 
   def parseRawCode(self):
     raise NotImplementedError("parseRawCode not implemented in virtual class")

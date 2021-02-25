954803cca4a1a527fbf3648a886b25cf42f4c156
Pierre Amadio <amadio.pierre@gmail.com>
2021-02-25 09:50:34 +0100

    cosmetic

--- bin/verb.py
+++ bin/verb.py
@@ -2,39 +2,39 @@
 """
 https://www.translatum.gr/converter/beta-code.htm
 
-V = Verbs (up to 3 cols.)  
-(I in col.3 = augmented, whatever the stem)   
+V = Verbs (up to 3 cols.)
+(I in col.3 = augmented, whatever the stem)
 "Progressive" Stems
-     V1 = regular present     
+     V1 = regular present
      V2 = contracts in -EW
-     V3 =   "       in -AW  
-     V4 =   "       in -OW  
-     V5 = regular -MI verbs 
-     V6 = -A stem -MI verbs   
-     V7 = -E stem  "    "   
-     V8 = -O stem  "    "     
-     V9 = EI)MI/ and EI)=MI   
-Aorist Stems               
-     VA = 1st aorist active   
+     V3 =   "       in -AW
+     V4 =   "       in -OW
+     V5 = regular -MI verbs
+     V6 = -A stem -MI verbs
+     V7 = -E stem  "    "
+     V8 = -O stem  "    "
+     V9 = EI)MI/ and EI)=MI
+Aorist Stems
+     VA = 1st aorist active
      VB = 2nd aorist act. #1
-     VZ = 2nd   "     "   #2(irreg) 
-     VH, VE, VO = -H, -E, -O stem -MI verbs    
+     VZ = 2nd   "     "   #2(irreg)
+     VH, VE, VO = -H, -E, -O stem -MI verbs
      VC = #1 aor.& fut.pass.(Q-type)
      VD = #2  "  "  "   "   (non-Q) 
-     VV = labial " " "  "     
+     VV = labial " " "  "
      VS = dental " " "  " (+ zeta)
-     VQ = guttural" " " "     
-Perfect Stems (I in col.3 = Plupf.augm.) 
-     VX = perfect active  
-     VM =   "     middle    
-     VP = labial perf. midd.  
+     VQ = guttural" " " "
+Perfect Stems (I in col.3 = Plupf.augm.)
+     VX = perfect active
+     VM =   "     middle
+     VP = labial perf. midd.
      VT = dental  "     " (+ zeta)
-     VK = guttural "     "  
-Future Stems               
-     VF = regular future  
+     VK = guttural "     "
+Future Stems
+     VF = regular future
      VF2 = liquid type(+ zeta)
-     VF3 = E)LAU/NW type    
-     VFX = future perfect 
+     VF3 = E)LAU/NW type
+     VFX = future perfect
 """
 
 from packard import *

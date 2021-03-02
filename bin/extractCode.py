42ea8c3f8d9564d4b7cd23e8452310195b902643
Pierre Amadio <amadio.pierre@gmail.com>
2021-03-02 09:35:30 +0100

    extractCode comment

--- bin/extractCode.py
+++ bin/extractCode.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import sys
-
+"""
+This script extract all morphcode mentionned in <w> node in the osis file 
+used as argument.
+"""
 dic=set()
-
-#osisLXX="/home/melmoth/dev/LXX/lxx.osis.xml"
 osisLXX=sys.argv[1]
 
 with open(osisLXX) as fp:
